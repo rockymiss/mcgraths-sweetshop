d1b6a089883115a8443060ffd9ea31227f8c4d21
rockymiss <rachelmcdermottrock@gmail.com>
2022-12-01 22:28:37 +0000

    Add view and url for product detail

--- products/urls.py
+++ products/urls.py
@@ -6,4 +6,5 @@ from . import views
 
 urlpatterns = [
     path('products/', views.ProductList.as_view(), name="products"),
+    path('products/<int:pk>', views.ProductDetail.as_view(), name="product_detail")
 ]
--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView
+from django.shortcuts import render, reverse, get_object_or_404
+from django.views.generic import ListView, DetailView
 from django.views import generic, View
 from .models import Products
 
@@ -13,3 +13,15 @@ class ProductList(generic.ListView):
     queryset = Products.objects.order_by('product_name')
     template_name = 'products/all_products.html'
     context_object_name = "products"
+
+
+class ProductDetail(View):
+    """
+    Shows individual products on their own page
+    """
+
+    model = Products
+    template_name = 'products/product_detail.html'
+
+    def get_success_url(self):
+        return reverse('product_detail', kwargs={'pk': self.object.id})
