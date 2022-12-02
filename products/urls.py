65dd9fcf0ad0ecbdae47a4f41fe0e8812e88fdeb
rockymiss <rachelmcdermottrock@gmail.com>
2022-12-02 19:00:46 +0000

    Add basic product detail to include updating pk in view and url

--- products/urls.py
+++ products/urls.py
@@ -6,5 +6,5 @@ from . import views
 
 urlpatterns = [
     path('products/', views.ProductList.as_view(), name="products"),
-    path('products/<int:pk>', views.ProductDetail.as_view(), name="product_detail")
+    path('product_detail.html/<int:pk>/', views.ProductDetail.as_view(), name="product_detail")
 ]
