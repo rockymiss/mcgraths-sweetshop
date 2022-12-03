2c1895c11f76c28c228ef5f5cd42aefe390e1139
rockymiss <rachelmcdermottrock@gmail.com>
2022-12-03 14:31:51 +0000

    Finalise search functionality by name or description

--- products/views.py
+++ products/views.py
@@ -6,29 +6,38 @@ from django.views import generic, View
 from .models import Products
 
 
-class ProductList(generic.ListView):
+class ProductList(ListView):
     """
-    Creates a list of blogs with status of posted
-    using the BlogPost Model
+    Displays a list of all products and provides a 
+    search functionality to display products that the 
+    user has searched for
     """
     model = Products
     template_name = 'products/all_products.html'
-    context_object_name = "products"
+    queryset = Products.objects.all()
 
     def get_queryset(self):
         """
-        overrides the get_queryset method and filters the product by what the 
-        user typed in to search
+        Overrides queryset and allows user to search
+        by name or description of product
         """
-        search = self.request.GET.get('q')
-        products = Q(product_name__icontains=search) | Q(description__icontains=search)
-        return Products.objects.filter(products)
+        query = self.request.GET.get('q')
+
+        if query:
+            object_list = self.model.objects.filter(
+                Q(product_name__icontains=query) |
+                Q(description__icontains=query)
+            )
+        else:
+            object_list = Products.objects.all()
+
+        return object_list
 
 
 class ProductDetail(View):
     """
-    This class will display the product the user selects from
-    the Product list.
+    Displays the product on it's own that the user selects 
+    from the Product list.
     """
 
     def get(self, request, pk, *args, **kwargs):
