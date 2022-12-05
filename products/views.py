f88167e88c2957f5c6833c897ccf76109b77e06d
rockymiss <rachelmcdermottrock@gmail.com>
2022-12-05 23:53:12 +0000

    Change all products view from class based to function

--- products/views.py
+++ products/views.py
@@ -1,37 +1,75 @@
-from django.shortcuts import render, reverse, get_object_or_404
-from django.views.generic import ListView, DetailView
+from django.shortcuts import render, reverse, redirect, get_object_or_404
+from django.views.generic import DetailView
 from django.contrib import messages
 from django.db.models import Q
 from django.views import generic, View
 from .models import Products
 
 
-class ProductList(ListView):
+def product_list(request):
     """
-    Displays a list of all products and provides a 
-    search functionality to display products that the 
-    user has searched for
+    Displays a list of all products, provides a
+    search functionality to display products that the
+    user has searched for and provides a way for the user
+    to filter by some categories.  Followed Boutique Ado
+
     """
-    model = Products
-    template_name = 'products/all_products.html'
-    queryset = Products.objects.all()
 
-    def get_queryset(self):
-        """
-        Overrides queryset and allows user to search
-        by name or description of product
-        """
-        query = self.request.GET.get('q')
+    products = Products.objects.all()
+    query = None
 
-        if query:
-            object_list = self.model.objects.filter(
-                Q(product_name__icontains=query) |
-                Q(description__icontains=query)
-            )
-        else:
-            object_list = Products.objects.all()
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "Why didn't you search for anything?")
+                return redirect(reverse('products'))
 
-        return object_list
+            query = Q(
+                name__icontains=query
+                ) | Q(description__icontains=query)
+            products = products.filter(query)
+
+    context = {
+        'products': products,
+        'search_term': query,
+    }
+
+    return render(request, 'products/all_products.html', context)
+
+
+# class ProductList(ListView):
+#     """
+#     Displays a list of all products and provides a 
+#     search functionality to display products that the 
+#     user has searched for
+#     """
+#     model = Products
+#     template_name = 'products/all_products.html'
+#     queryset = Products.objects.all()
+
+#     def get_queryset(self):
+#         """
+#         Overrides queryset and allows user to search
+#         by name or description of product
+#         """
+#         query = self.request.GET.get('q')
+#         category = self.request.GET.get('category')
+
+#         if category:
+#             object_list = self.model.objects.filter(
+#                 Q(category__icontains=category) 
+#             )
+
+#         if query:
+#             object_list = self.model.objects.filter(
+#                 Q(product_name__icontains=query) |
+#                 Q(description__icontains=query)
+#             )
+#         else:
+#             object_list = Products.objects.all()
+
+#         return object_list
 
 
 class ProductDetail(View):
