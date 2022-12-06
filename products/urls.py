bcaa731d729d191c14ae1ae0226aea4271370290
rockymiss <rachelmcdermottrock@gmail.com>
2022-12-06 20:50:50 +0000

    Add url for offers

--- products/urls.py
+++ products/urls.py
@@ -6,5 +6,6 @@ from . import views
 
 urlpatterns = [
     path('products/', views.product_list, name="products"),
-    path('<product_id>', views.ProductDetail.as_view(), name="product_detail")
+    path('<product_id>', views.ProductDetail.as_view(), name="product_detail"),
+    path('products/offers/', views.offers, name="offers")
 ]
