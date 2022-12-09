04f4582ae49f06d3b0fca886a67b7d12b4514db6
rockymiss <rachelmcdermottrock@gmail.com>
2022-12-09 00:32:10 +0000

    Fix minor error on threshold variable in contexts

--- cart/contexts.py
+++ cart/contexts.py
@@ -27,7 +27,7 @@ def cart_contents(request):
         'product_count': product_count,
         'delivery': delivery,
         'free_delivery_delta': free_delivery_delta,
-        'free_delivery_threshold': free_delivery_threshold,
+        'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
         'grand_total': grand_total
     }
 
