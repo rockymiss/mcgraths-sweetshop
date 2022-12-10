4555a574ec23e821da9ebb51bd5bf1421388bb64
rockymiss <rachelmcdermottrock@gmail.com>
2022-12-10 23:42:37 +0000

    Add views to delete cart, update urls and templates

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from products.models import Products
 
 # Create your views here.
 
@@ -17,6 +18,7 @@ def add_to_cart(request, item_id):
     browsing the website elsewhere
     """
 
+    product = get_object_or_404(Products, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
@@ -29,3 +31,22 @@ def add_to_cart(request, item_id):
     request.session['cart'] = cart
     return redirect(redirect_url)
 
+
+def adjust_cart(request, item_id):
+    """
+    Adjusts the quantity from the cart template
+    """
+
+    quantity = int(request.POST.get('quantity', 0))
+    redirect_url = request.POST.get('redirect_url')
+    cart = request.session.get('cart', {})
+
+    if quantity > 0:
+        cart[item_id] = quantity
+        print(quantity)
+    else:
+        cart.pop(item_id)
+        print(quantity)
+
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
