c6d7282a1131e0a96a9e1c17a24255a91d687b79
rockymiss <rachelmcdermottrock@gmail.com>
2023-01-08 20:12:07 +0000

    Add messages for user re cart changes

--- cart/views.py
+++ cart/views.py
@@ -19,7 +19,7 @@ def add_to_cart(request, item_id):
     Also allows the user to tore the contents of the Cart when 
     browsing the website elsewhere
     """
-    product = Products.objects.get(pk=item_id)
+    product = get_object_or_404(Products, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     colour = None
@@ -32,18 +32,21 @@ def add_to_cart(request, item_id):
         if item_id in list(cart.keys()):
             if colour in cart[item_id]['items_by_colour'].keys():
                 cart[item_id]['items_by_colour'][colour] += quantity
+                messages.success(request, f'You added {cart[item_id]["items_by_colour"][colour]} {product.name} with the colour {colour.upper()} to your cart')
             else:
                 cart[item_id]['items_by_colour'][colour] = quantity
+                messages.success(request, f'You added {product.name} with the colour {colour.upper()} to your cart ')
         else:
             cart[item_id] = {'items_by_colour': {colour: quantity}}
+            messages.success(request, f'You added {product.name} {colour.upper()} to your cart ')
     else:
         if item_id in list(cart.keys()):
             cart[item_id] += quantity
-            messages.success(request, f'Updated {product.name} quantity to {cart[item_id]} in your cart ')
+            messages.success(request, f'Updated {cart[item_id]} {product.name} in your cart ')
 
         else:
             cart[item_id] = quantity
-            messages.success(request, f'Added {product.name} to your cart ')
+            messages.success(request, f'You Added {product.name} to your cart ')
 
     request.session['cart'] = cart
     return redirect(redirect_url)
@@ -53,7 +56,7 @@ def adjust_cart(request, item_id):
     """
     Adjusts the quantity from the cart template
     """
-
+    product = get_object_or_404(Products, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     colour = None
     if 'product_colour' in request.POST:
@@ -63,15 +66,19 @@ def adjust_cart(request, item_id):
     if colour:
         if quantity > 0:
             cart[item_id]['items_by_colour'][colour] = quantity
+            messages.success(request, f'There are now {cart[item_id]["items_by_colour"][colour]} {product.name} with the colour {colour.upper()} in your cart')
         else:
             del cart[item_id]['items_by_colour'][colour]
             if not cart[item_id]['items_by_colour']:
                 cart.pop(item_id)
+                messages.success(request, f'You removed {product.name} with the colour {colour.upper()} to your cart ')
     else:
         if quantity > 0:
             cart[item_id] = quantity
+            messages.success(request, f'There are now {cart[item_id]} {product.name} in your cart ')
         else:
             cart.pop(item_id)
+            messages.success(request, f'You removed {product.name} from your cart ')
 
     request.session['cart'] = cart
     return redirect(reverse('view_cart'))
@@ -82,6 +89,7 @@ def remove_cart(request, item_id):
     Removes items from the cart
     """
     try:
+        product = get_object_or_404(Products, pk=item_id)
         colour = None
         if 'product_colour' in request.POST:
             colour = request.POST['product_colour']
@@ -92,11 +100,14 @@ def remove_cart(request, item_id):
                 del cart[item_id]['items_by_colour'][colour]
                 if not cart[item_id]['items_by_colour']:
                     cart.pop(item_id)
+                messages.success(request, f'You removed {cart[item_id]["items_by_colour"][colour]} {product.name} with the colour {colour.upper()} from your cart')
         else:
             cart.pop(item_id)
+            messages.success(request, f'You removed {product.name} from your cart')
 
         request.session['cart'] = cart
         return redirect(reverse('view_cart'))
   
     except Exception as e:
+        messages.error(request, f'There was an error removing item: {e} from the cart')
         return HttpResponse(status=500)
