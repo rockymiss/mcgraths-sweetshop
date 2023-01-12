d58adba080cc68b2debf298adc601ff8bb74c436
rockymiss <rachelmcdermottrock@gmail.com>
2023-01-12 20:38:30 +0000

    Testing stripe keys - save for another power cut

--- checkout/views.py
+++ checkout/views.py
@@ -75,27 +75,29 @@ def checkout(request):
                     return redirect(reverse('view_cart'))
             request.session['save_info'] = 'save-info' in request.POST
 
-            return redirect(reverse('checkout_success', 
+            return redirect(reverse('checkout_success',
                                     args=[order.order_number]))
         else:
             messages.error(request, "There was somthing wrong with your form!")
     else:
+        cart = request.session.get('cart', {})
+        if not cart:
+            messages.error(request, "There's nothing to see here!")
+            return redirect(reverse('products'))
 
-
-    # cart = request.session.get('cart', {})
-    # if not cart:
-    #     messages.error(request, "There's nothing to see here!")
-    #     return redirect(reverse('products'))
-
-    # current_cart = cart_contents(request)
-    # total = current_cart['grand_total']
-    # stripe_total = round(total * 100)
-    # stripe.api_key = stripe_secret_key
-    # intent = stripe.PaymentIntent.create(
-    #         amount=stripe_total,
-    #         currency=settings.STRIPE_CURRENCY,
-    #     )
-
+        current_cart = cart_contents(request)
+        total = current_cart['grand_total']
+        stripe_total = round(total * 100)
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
+                amount=stripe_total,
+                currency=settings.STRIPE_CURRENCY,
+            )
+        print(intent)
+        print("This is the public key")
+        print(stripe_public_key)
+        print("This is the secret key")
+        print(stripe_secret_key)
         order_form = OrderForm()
 
     if not stripe_public_key:
@@ -118,27 +120,6 @@ def checkout_success(request, order_number):
     """
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
-
-    # if request.user.is_authenticated:
-    #     profile = UserProfile.objects.get(user=request.user)
-    #     # Attach the user's profile to the order
-    #     order.user_profile = profile
-    #     order.save()
-
-    #     # Save the user's info
-    #     if save_info:
-    #         profile_data = {
-    #             'default_phone_number': order.phone_number,
-    #             'default_country': order.country,
-    #             'default_postcode': order.postcode,
-    #             'default_town_or_city': order.town_or_city,
-    #             'default_street_address1': order.street_address1,
-    #             'default_street_address2': order.street_address2,
-    #             'default_county': order.county,
-    #         }
-    #         user_profile_form = UserProfileForm(profile_data, instance=profile)
-    #         if user_profile_form.is_valid():
-    #             user_profile_form.save()
 
     messages.success(request, f'Order successfully processed! \
         Your order number is {order_number}. A confirmation \
