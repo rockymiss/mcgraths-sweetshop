c4c2d4ab72b2b4ea15b133852b7d9fd7eb15c54a
rockymiss <rachelmcdermottrock@gmail.com>
2023-02-14 16:36:25 +0000

    Install django countries and update Order model

--- checkout/models.py
+++ checkout/models.py
@@ -4,6 +4,8 @@ from django.db import models
 from django.db.models import Sum
 from django.conf import settings
 
+from django_countries.fields import CountryField
+
 from products.models import Products
 
 # Create your models here.
@@ -19,7 +21,8 @@ class Order(models.Model):
     full_name = models.CharField(max_length=55, null=False, blank=False)
     email = models.EmailField(max_length=255, null=False, blank=False)
     phone_number = models.CharField(max_length=22, null=False, blank=False)
-    country = models.CharField(max_length=40, null=False, blank=False)
+    country = CountryField(max_length=40, blank_label='Country *',
+                           null=False, blank=False)
     postcode = models.CharField(max_length=20, null=True, blank=True)
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
     address1 = models.CharField(max_length=80, null=False, blank=False)
@@ -33,7 +36,8 @@ class Order(models.Model):
     grand_total = models.DecimalField(
         max_digits=10, decimal_places=2, null=False, default=0)
     original_cart = models.TextField(null=False, blank=False, default='')
-    stripe_pid = models.CharField(max_length=254, null=False, blank=False, default="")
+    stripe_pid = models.CharField(max_length=254, null=False,
+                                  blank=False, default="")
 
     def _generate_order_number(self):
         """
