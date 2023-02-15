c1de80e7bd76e17d903167ea6324dbac64a1be6a
rockymiss <rachelmcdermottrock@gmail.com>
2023-02-15 22:26:01 +0000

    Add profile form, views and update template

--- user_profiles/models.py
+++ user_profiles/models.py
@@ -18,14 +18,14 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     default_phone_number = models.CharField(max_length=22, null=True,
                                             blank=True)
-    default_country = CountryField(max_length=100, blank_label='Country *',
-                                   null=True, blank=True)
-    default_postcode = models.CharField(max_length=20, null=True, blank=True)
-    default_town_or_city = models.CharField(max_length=40, null=True,
-                                            blank=True)
     default_address1 = models.CharField(max_length=80, null=True, blank=True)
     default_address2 = models.CharField(max_length=80, null=True, blank=True)
+    default_town_or_city = models.CharField(max_length=40, null=True,
+                                            blank=True)
+    default_postcode = models.CharField(max_length=20, null=True, blank=True)    
     default_county = models.CharField(max_length=80, null=True, blank=True)
+    default_country = CountryField(max_length=100, blank_label='Country',
+                                   null=True, blank=True)
 
     def __str__(self):
         return self.user.username
