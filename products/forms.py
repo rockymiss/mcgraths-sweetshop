20f0f4f3707fab166a561ef3a71ceb667abe3e05
rockymiss <rachelmcdermottrock@gmail.com>
2023-02-16 22:55:33 +0000

    Create form for superuser to enter products

---
+++ products/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from .models import Products, Category
+
+
+class ProductForm(forms.ModelForm):
+    """
+    Form for superuser to update stock
+    """
+
+    class Meta:
+        model = Products
+        fields - '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        categories = Category.objects.all()
+        friendly_names - [(c.id, c.get_friendly_name()) for c in categories]
+
+        self.fields['category'].choices = friendly_names
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
+            
