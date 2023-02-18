f43db06075884d37512eb24a6a8a4a1641269eae
rockymiss <rachelmcdermottrock@gmail.com>
2023-02-18 15:58:18 +0000

    Bug Fixed update forms.py to include product_image instead of image

--- products/forms.py
+++ products/forms.py
@@ -13,8 +13,8 @@ class ProductForm(forms.ModelForm):
         model = Products
         fields = '__all__'
 
-    image = forms.ImageField(label='Image', required=False,
-                             widget=CustomClearableFileInput)
+    product_image = forms.ImageField(label='Image', required=False,
+                                     widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
