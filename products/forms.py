e96bbe8765b03091c81f5db57a53fd9566df23cf
rockymiss <rachelmcdermottrock@gmail.com>
2023-02-18 16:38:06 +0000

    Update image field when adding and editing products

--- products/forms.py
+++ products/forms.py
@@ -13,7 +13,7 @@ class ProductForm(forms.ModelForm):
         model = Products
         fields = '__all__'
 
-    image = forms.ImageField(label='Image', required=False,
+    product_image = forms.ImageField(label='Image', required=False,
                              widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
